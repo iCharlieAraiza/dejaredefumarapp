6ddf9ad5e7bf610ede25db61ca7760c841f6e772
iCharlieAraiza <eliptikman1234@gmail.com>
2023-09-27 00:21:51 -0600

    Fix hero svg line

--- src/components/HomepageDescription.tsx
+++ src/components/HomepageDescription.tsx
@@ -55,6 +55,20 @@ const HomepageDescription = () => {
         */}
           </div>
         </div>
+        <div className="top-app_description" style={{'transform':'rotate(180deg)', 'marginTop':'-10px'}}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1437"
+              height="38"
+              fill="none"
+              viewBox="0 0 1437 38"
+            >
+              <path
+                fill="#F2EEE1"
+                d="M26.5 18.393H0v19.5h1437v-29.5h-96l-58.5 10c-9 3-35.2 5.2-68-10s-116-6.334-153.5 0l-106 10h-73.5l-75.5-10H623.5l-116.5 19-99-19H275.5l-60.5 10c-27-3.334-91-10-131-10s-55 6.666-57.5 10z"
+              ></path>
+            </svg>
+          </div>
         <div className="container">
           <div className="section-homepage__description">
             <div className="section-homepage__column">
